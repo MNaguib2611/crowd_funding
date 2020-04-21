562efd15dc04e3bc1bc1956fe57c8fda3e1bf517
AhmedAdel <ahmedadelfahim@gmail.com>
2020-04-21 20:25:41 +0200

    latest projects home

--- users/views.py
+++ users/views.py
@@ -14,10 +14,16 @@ from django.core import serializers
 def home(req):
     # print(type(req.GET.get('cat_id')))
     categories = Category.objects.all()
+    latest_projects = Project.objects.all().order_by('-start_date')[:5]
+    paginator_latest_projects = Paginator(latest_projects, 3)  # Show 25 contacts per page.
+    page_number_latest_projects = req.GET.get('page_lat_pro')
+    page_obj_latest_projects = paginator_latest_projects.get_page(page_number_latest_projects)
+
     projects_by_category = get_projects_by_category(req.GET.get('cat_id'))
-    paginator_category = Paginator(projects_by_category, 1)  # Show 25 contacts per page.
+    paginator_category = Paginator(projects_by_category, 3)  # Show 25 contacts per page.
     page_number = req.GET.get('page_cat')
     page_obj = paginator_category.get_page(page_number)
+
     context = {
         "projects_by_category":
             {'page_obj': page_obj,
@@ -25,9 +31,12 @@ def home(req):
         "categories": {
             "curr_category": int(req.GET.get('cat_id')) if (req.GET.get('cat_id'))  else 0,
             "categories":categories
-        }
+        },
+        "latest_projects":
+            {'page_obj': page_obj_latest_projects,
+             'projects': page_obj_latest_projects.object_list},
     }
-    print(context)
+    # print(context)
     return render(req, 'users/home.html',context)
 
 
@@ -38,3 +47,4 @@ def get_projects_by_category(id):
         projects = Project.objects.filter(category_id=int(id))
     return projects
     # return JsonResponse(data)
+
