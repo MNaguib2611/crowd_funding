c34744bdba24e0e56a97bc849a4daad4d4b6e206
Mohammed Naguib <m.naguib2611@gmail.com>
2020-04-30 01:49:40 +0200

    removed-unused-packages

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import render, redirect
 
 from django.views.generic import View
 from django.contrib import messages
-from validate_email import validate_email
+# from validate_email import validate_email
 #from django.contrib.auth.models  import User, auth
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms  import UserCreationForm
