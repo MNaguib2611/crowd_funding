07db742f0f7c6d5e62366597450cf7dc4b1ab20d
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-04-30 16:32:39 +0200

    fix conflicts

--- users/views.py
+++ users/views.py
@@ -304,13 +304,8 @@ def update_project(request, project_id):
         start_date=start_date, end_date=end_date, category_id=category_id)
     return redirect(user_projects)
     
-<<<<<<< HEAD
 
-
-
-=======
 @login_required
->>>>>>> 13518003f6487e77de39739a0bbcb03dbfa3ab0b
 def home(req):
     user_id = req.session['user_id']
     categories = Category.objects.all()
